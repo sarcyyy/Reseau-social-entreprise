b053d0549ca1886e91db81b37d9b160d1dff10c5
Anthony <anthony.bano.34@gmail.com>
2022-08-30 01:25:22 +0200

    all component made

---
+++ Projet7-master/src/components/Listederoulante.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Listederoulante = () => {
+    return (
+        <div className='hidden'>
+            <ul>
+                <li>A</li>
+                <li>A</li>
+                <li>A</li>
+                <li>A</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Listederoulante;
--- Projet7-master/src/components/Profilepic.js
+++ Projet7-master/src/components/Profilepic.js
@@ -1,12 +1,26 @@
 import React from "react";
+// import { useState } from "react";
+import Listederoulante from "./Listederoulante";
 
 const Profilepic = () => {
+  // const [Isactive, setIsactive] = useState(false);
   return (
+    <div>
     <div className="avatar">
-      <img src="/img/logo192.png" alt="profil" className="picsize" />
+      <img src="/img/logo192.png" alt="profil" className="picsize" onClick={() => { 
+        // setIsactive(true);   
+      } }/>
       <p>Nom de la personne</p>
+      </div>
+         {/* { Isactive ? <Listederoulante /> : ""} */}
+<Listederoulante />
+      
+      
     </div>
+    
   );
+
 };
 
 export default Profilepic;
+
