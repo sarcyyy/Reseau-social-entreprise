24982522b57559da8c13605dfa037fe517c2da82
Anthony <anthony.bano.34@gmail.com>
2022-09-18 01:42:25 +0200

    changing tokenexpire

--- Projet7-master/backend/controllers/user.js
+++ Projet7-master/backend/controllers/user.js
@@ -34,7 +34,7 @@ exports.login = (req, res, next) => {
           res.status(200).json({
             userId: user._id,
             token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
-              expiresIn: "24h",
+              expiresIn: "500 days",
             }),
             name: user.name, // verifier que j'ai bien mis user.name
           });
