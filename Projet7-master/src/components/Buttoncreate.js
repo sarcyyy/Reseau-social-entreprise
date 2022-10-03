142e91c0baedf1bd05d2eafdd378e196a2e24b02
Anthony <anthony.bano.34@gmail.com>
2022-10-03 14:51:29 +0200

    making regex for creating account

--- Projet7-master/src/components/Buttoncreate.js
+++ Projet7-master/src/components/Buttoncreate.js
@@ -6,31 +6,47 @@ import islogged from "../script/Islogged";
 const Buttoncreate = ({ createemail, createpassword, createname }) => {
   const testtoken = localStorage.getItem("token");
   const fetchcreate = (e) => {
-    var login = {
-      email: createemail,
-      password: createpassword,
-      name: createname,
-    };
-    fetch("http://localhost:3000/api/auth/signup", {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-      body: JSON.stringify(login),
-    }).then((rep) => {
-      console.log(rep);
-      if (rep.ok === true) {
-        console.log("Utilisateur créer");
-        setIslogtrue(true);
-        alert("veuillez vous connecter avec vos identifiants");
+    let regexpassword = new RegExp("^[a-zA-Z -]{3,}$");
+    let regexemail = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+    if (
+      regexemail.test(createemail) === false ||
+      regexpassword.test(createpassword) === false
+    ) {
+      if (regexemail.test(createemail) === false) {
+        alert("veuillez entrer une adresse email valide");
       } else {
-        if (rep.ok === false) {
-          alert("mauvaise combinaison");
-        }
+        alert(
+          "Veuillez verifier que le mot de passe contient plus de 3 caractères."
+        );
       }
-    });
+    } else {
+      var login = {
+        email: createemail,
+        password: createpassword,
+        name: createname,
+      };
+      fetch("http://localhost:3000/api/auth/signup", {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(login),
+      }).then((rep) => {
+        console.log(rep);
+        if (rep.ok === true) {
+          console.log("Utilisateur créer");
+          setIslogtrue(true);
+          alert("veuillez vous connecter avec vos identifiants");
+        } else {
+          if (rep.ok === false) {
+            alert("mauvaise combinaison");
+          }
+        }
+      });
+    }
   };
+
   if (islogged(testtoken) === true) {
     window.location = "http://localhost:7200/accueil";
   }
